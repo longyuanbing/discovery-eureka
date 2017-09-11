fdc15c3521c596a4643ad5c3190fc81aac1eaa9b
longyuanbing <longyuanbing@lifesense.com>
2017-09-11 20:23:52 +0800

    first commit

---
+++ src/main/java/com/yuandu/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.yuandu.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka做服务发现.
+ *
+ * @author
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
